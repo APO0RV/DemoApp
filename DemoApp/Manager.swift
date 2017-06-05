ad08e8e5403b1b07b11fefcccc0d3e149cc30421
Apoorv <asrivastava@gauselmann.com>
2017-06-05 12:17:18 +0530

    Manager added

---
+++ DemoApp/Manager.swift
@@ -0,0 +1,13 @@
+//
+//  Manager.swift
+//  DemoApp
+//
+//  Created by Apoorv Srivastava on 05/06/17.
+//  Copyright © 2017 MGI. All rights reserved.
+//
+
+import UIKit
+
+class Manager: NSObject {
+
+}
